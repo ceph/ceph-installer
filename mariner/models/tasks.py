156dc43ef22d7e2920e0909964a9c081c7dc9c11
Alfredo Deza <adeza@redhat.com>
2016-02-01 17:10:39 -0500

    create a tasks model
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ mariner/models/tasks.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column, Integer, String,  Boolean, DateTime
+from sqlalchemy.orm.exc import DetachedInstanceError
+from mariner.models import Base
+
+
+class Task(Base):
+
+    __tablename__ = 'tasks'
+    id = Column(Integer, primary_key=True)
+    identifier = Column(String(256), unique=True, nullable=False, index=True)
+    endpoint = Column(String(256), index=True)
+    command = Column(String(256))
+    stderr = Column(String(256))
+    stdout = Column(String(256))
+    started = Column(DateTime)
+    ended = Column(DateTime)
+    succeeded = Column(Boolean(), default=False)
+
+    def __repr__(self):
+        try:
+            return '<Task %r>' % self.name
+        except DetachedInstanceError:
+            return '<Task detached>'
+
+    def __json__(self):
+        return dict(
+            identifier = self.identifier,
+            endpoint = self.endpoint,
+            command = self.command,
+            stderr = self.stderr,
+            stdout = self.stdout,
+            started = selfstarted,
+            ended = self.ended,
+            succeeded = self.succeeded,
+        )
