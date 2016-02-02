377d130e4305880b7d0cf846adc24c1e6e969f38
Alfredo Deza <adeza@redhat.com>
2016-02-02 13:56:51 -0500

    create a populate command for the database
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ mariner/commands/populate.py
@@ -0,0 +1,31 @@
+from pecan.commands.base import BaseCommand
+from pecan import conf
+
+from mariner import models
+
+
+def out(string):
+    print "==> %s" % string
+
+
+class PopulateCommand(BaseCommand):
+    """
+    Load a pecan environment and initializate the database.
+    """
+
+    def run(self, args):
+        super(PopulateCommand, self).run(args)
+        out("LOADING ENVIRONMENT")
+        self.load_app()
+        out("BUILDING SCHEMA")
+        try:
+            out("STARTING A TRANSACTION...")
+            models.start()
+            models.Base.metadata.create_all(conf.sqlalchemy.engine)
+        except:
+            models.rollback()
+            out("ROLLING BACK... ")
+            raise
+        else:
+            out("COMMITING... ")
+            models.commit()
