76934ee7087fd5b979b535e8cf4263042ff65a97
Alfredo Deza <adeza@redhat.com>
2016-02-03 16:55:30 -0500

    make the mon controller be able to install
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- mariner/controllers/mon.py
+++ mariner/controllers/mon.py
@@ -1,5 +1,8 @@
-from pecan import expose, abort
+import os
+from pecan import expose, request
+from uuid import uuid4
 from mariner.controllers import error
+from mariner import process, models, util
 
 
 class MONController(object):
@@ -17,6 +20,20 @@ class MONController(object):
     # we need validation here
     @install.when(method='POST', template='json')
     def install_post(self):
+        identifier = uuid4()
+        # VALIDATIONNNNNN!!!! We can't do this without hosts
+        hosts = request.json.get('hosts')
+        hosts_file = util.generate_inventory_file('mons', hosts, identifier)
+        tag = 'package-install'
+        stdout = process.temp_file(identifier, 'stdout')
+        stderr = process.temp_file(identifier, 'stderr')
+        command = process.make_ansible_command(stderr, stdout, hosts_file, identifier, tag=tag)
+        task = models.Task(
+            identifier=identifier,
+            endpoint=request.path,
+            command=command
+        )
+        os.system(command)
         return {}
 
     @expose(generic=True, template='json')
