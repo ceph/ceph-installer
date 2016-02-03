57b95ae7efd25a07d28be5009171d4411c068f29
Alfredo Deza <adeza@redhat.com>
2016-02-03 16:55:14 -0500

    create some utilities for running system commands
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- mariner/util.py
+++ mariner/util.py
@@ -22,3 +22,43 @@ def generate_inventory_file(group_name, hosts, task_uuid, tmp_dir=None):
     inventory_file.write(result_str)
     inventory_file.close()
     return inventory_file.name
+
+
+def api_endpoint(endpoint=None, *args):
+    """
+    Puts together the API url for mita, so that we can talk to it. Optionally, the endpoint
+    argument allows to return the correct url for specific needs. For example, to create a node:
+
+        http://0.0.0.0/api/nodes/
+
+    """
+    server = pecan.conf['server']['host']
+    port = pecan.conf['server']['port']
+    base = "http://%s:%s/api" % (server, port)
+    endpoints = {
+        'nodes': '%s/nodes/' % base,
+    }
+    url = base
+    if endpoint:
+        url = endpoints[endpoint]
+
+    if args:
+        for part in args:
+            url = os.path.join(url, part)
+    return url
+
+
+def which(executable):
+    locations = (
+        '/usr/local/bin',
+        '/bin',
+        '/usr/bin',
+        '/usr/local/sbin',
+        '/usr/sbin',
+        '/sbin',
+    )
+
+    for location in locations:
+        executable_path = os.path.join(location, executable)
+        if os.path.exists(executable_path):
+            return executable_path
