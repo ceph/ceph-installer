09836fdec51c13734d5791883d45571dab817078
Andrew Schoen <aschoen@redhat.com>
2016-02-04 15:24:17 -0600

    a couple more tests for /api/mon/install/ validation
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- mariner/tests/controllers/test_mon.py
+++ mariner/tests/controllers/test_mon.py
@@ -5,7 +5,19 @@ class TestMonController(object):
         result = session.app.get("/api/mon/")
         assert result.status_int == 200
 
-    def test_install_incorrect_json(self, session):
+    def test_install_missing_hosts(self, session):
         result = session.app.post_json("/api/mon/install/", params=dict(),
+                                       expect_errors=True)
+        assert result.status_int == 400
+
+    def test_install_bogus_field(self, session):
+        data = dict(hosts=["google.com"], bogus="foo")
+        result = session.app.post_json("/api/mon/install/", params=data,
+                                       expect_errors=True)
+        assert result.status_int == 400
+
+    def test_install_hosts_not_a_list(self, session):
+        data = dict(hosts="google.com")
+        result = session.app.post_json("/api/mon/install/", params=data,
                                        expect_errors=True)
         assert result.status_int == 400
