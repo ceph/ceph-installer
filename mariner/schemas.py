81c4b28e6b556a5cbd0043465987f7eafa76a568
Andrew Schoen <aschoen@redhat.com>
2016-02-04 15:10:58 -0600

    adds the rest of the mon install schema fields per the README
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- mariner/schemas.py
+++ mariner/schemas.py
@@ -1,5 +1,11 @@
 from notario.validators import types
+from notario.decorators import optional
 
 mon_install_schema = (
+    (optional("adjust-repos"), types.boolean),
+    (optional("gpg-url"), types.string),
     ("hosts", types.array),
+    (optional("release"), types.string),
+    (optional("repo-only"), types.boolean),
+    (optional("repo-url"), types.string),
 )
