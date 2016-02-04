23103b005ee621bbec94d03596fb4bdd629b5539
Andrew Schoen <aschoen@redhat.com>
2016-02-04 16:08:43 -0600

    provide a custom error message for hosts validation at /mon/install/
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- mariner/schemas.py
+++ mariner/schemas.py
@@ -1,10 +1,14 @@
 from notario.validators import types
 from notario.decorators import optional
 
+
+def list_of_hosts(value):
+    assert isinstance(value, list), "Please provide a list of hosts in the format: ['host1', 'host2']"
+
 mon_install_schema = (
     (optional("adjust-repos"), types.boolean),
     (optional("gpg-url"), types.string),
-    ("hosts", types.array),
+    ("hosts", list_of_hosts),
     (optional("release"), types.string),
     (optional("repo-only"), types.boolean),
     (optional("repo-url"), types.string),
