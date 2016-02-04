6b07937e995aeb28d6ae21718e305e7aa62ecad5
Alfredo Deza <adeza@redhat.com>
2016-02-04 11:51:54 -0500

    init the models when configuring celery
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- mariner/async.py
+++ mariner/async.py
@@ -3,6 +3,7 @@ import pecan
 
 from celery import Celery
 from celery.signals import worker_init
+from mariner import models
 
 
 @worker_init.connect
@@ -11,9 +12,13 @@ def bootstrap_pecan(signal, sender):
         config_path = os.environ['PECAN_CONFIG']
     except KeyError:
         here = os.path.abspath(os.path.dirname(__file__))
+        # XXX this will not hold true when installing as a binary
         config_path = os.path.abspath(os.path.join(here, '../config/config.py'))
 
     pecan.configuration.set_config(config_path, overwrite=True)
+    # Once configuration is set we need to initialize the models so that we can connect
+    # to the DB wth a configured mapper.
+    models.init_model()
 
 
 app = Celery('mariner.async', broker='amqp://guest@localhost//', include=['mariner.tasks'])
