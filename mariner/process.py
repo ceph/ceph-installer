e707e62e0575e9cd3952dc8e0439ab317458e126
Alfredo Deza <adeza@redhat.com>
2016-02-04 08:14:32 -0500

    use the new playbook_path helper
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- mariner/process.py
+++ mariner/process.py
@@ -1,22 +1,24 @@
 import tempfile
 import logging
-from mariner.util import api_endpoint, which
+from mariner.util import api_endpoint, which, get_playbook_path
 
 logger = logging.getLogger(__name__)
 
 
-def make_ansible_command(stderr, stdout, hosts_file, identifier, extra_vars='{}', tag=''):
+def make_ansible_command(stderr, stdout, hosts_file, identifier, extra_vars='{}', tags=''):
     """
     This utility will compose the command needed to run ansible, capture its
     stdout and stderr to a file
     """
+    playbook = get_playbook_path()
     ansible_path = which('ansible-playbook')
     url = api_endpoint('tasks', identifier, 'completed')
-    command = '{ansible_path} -i {hosts_file} --extra-vars="{extra_vars}" --tags="{tags}"'.format(
+    command = '{ansible_path} -i {hosts_file} --extra-vars="{extra_vars}" --tags="{tags}" {playbook}'.format(
             ansible_path=ansible_path,
             hosts_file=hosts_file,
             extra_vars=extra_vars,
-            tags=tags
+            tags=tags,
+            playbook=playbook
     )
 
     redirect = '> {stdout} 2> {stderr};'.format(stdout=stdout, stderr=stderr)
@@ -28,4 +30,4 @@ def temp_file(identifier, std):
     return tempfile.NamedTemporaryFile(
         prefix="{identifier}_{std}".format(
             identifier=identifier, std=std), delete=False
-    )
+    ).name
