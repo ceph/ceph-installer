7a84f6d13f9cc4e5a23450b6ebb7b1175bec6630
Alfredo Deza <adeza@redhat.com>
2016-02-08 17:43:45 -0500

    some tests for the system hooks
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ mariner/tests/test_hooks.py
@@ -0,0 +1,42 @@
+import pytest
+from mariner import hooks
+
+
+class FakeState(object):
+
+    def __init__(self, **kw):
+        for k, v in kw.items():
+            setattr(self, k, v)
+
+
+class TestAnsibleExists(object):
+
+    def test_ansible_is_there(self, monkeypatch):
+        monkeypatch.setattr(
+            hooks, 'which',
+            lambda x: '/usr/bin/ansible-playbook')
+        assert hooks.ansible_exists() is None
+
+    def test_ansible_is_not_there(self, monkeypatch):
+        monkeypatch.setattr(
+            hooks, 'which',
+            lambda x: None)
+        with pytest.raises(hooks.SystemCheckError):
+            hooks.ansible_exists()
+
+
+class TestCeleryHasWorkers(object):
+
+    def test_celery_has_workers(self, monkeypatch):
+        stats = lambda: {'value': 'some stat'}
+        monkeypatch.setattr(
+            hooks, 'inspect',
+            lambda: FakeState(stats=stats))
+        assert hooks.celery_has_workers() is None
+
+    def test_celery_has_no_workers(self, monkeypatch):
+        monkeypatch.setattr(
+            hooks, 'inspect',
+            lambda: FakeState(stats=lambda: None))
+        with pytest.raises(hooks.SystemCheckError):
+            hooks.celery_has_workers()
