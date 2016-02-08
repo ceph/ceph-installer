d121d80bd29941de59e394587fa7a4d07a8fefd7
Andrew Schoen <aschoen@redhat.com>
2016-02-08 15:54:13 -0600

    implement the /api/osd/install/ endpoint
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- mariner/controllers/osd.py
+++ mariner/controllers/osd.py
@@ -1,4 +1,15 @@
-from pecan import expose
+import logging
+
+from pecan import expose, request
+from pecan.ext.notario import validate
+from uuid import uuid4
+
+from mariner.controllers import error
+from mariner.tasks import install
+from mariner import schemas
+from mariner import models
+
+logger = logging.getLogger(__name__)
 
 
 class OSDController(object):
@@ -9,9 +20,27 @@ class OSDController(object):
         # available
         return dict()
 
-    @expose('json')
+    @expose(generic=True, template='json')
     def install(self):
-        return {}
+        error(405)
+
+    @install.when(method='POST', template='json')
+    @validate(schemas.mon_install_schema, handler="/errors/schema")
+    def install_post(self):
+        hosts = request.json.get('hosts')
+        identifier = str(uuid4())
+        task = models.Task(
+            identifier=identifier,
+            endpoint=request.path,
+        )
+        # we need an explicit commit here because the command may finish before
+        # we conclude this request
+        models.commit()
+        install.apply_async(
+            ('osd', hosts, identifier),
+        )
+
+        return task
 
     @expose('json')
     def configure(self):
