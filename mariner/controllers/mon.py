9805fbf58aac15ee76178f9cbf9d24fcf580e329
Andrew Schoen <aschoen@redhat.com>
2016-02-10 11:14:48 -0600

    api/mon/install/ will not pass an extra_vars dict to install.task
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- mariner/controllers/mon.py
+++ mariner/controllers/mon.py
@@ -8,6 +8,7 @@ from mariner.controllers import error
 from mariner.tasks import install
 from mariner import schemas
 from mariner import models
+from mariner import util
 
 
 logger = logging.getLogger(__name__)
@@ -29,6 +30,7 @@ class MONController(object):
     @validate(schemas.install_schema, handler="/errors/schema")
     def install_post(self):
         hosts = request.json.get('hosts')
+        extra_vars = util.get_install_extra_vars(request.json)
         identifier = str(uuid4())
         task = models.Task(
             identifier=identifier,
@@ -37,8 +39,10 @@ class MONController(object):
         # we need an explicit commit here because the command may finish before
         # we conclude this request
         models.commit()
+        kwargs = dict(extra_vars=extra_vars)
         install.apply_async(
-            ('mon', hosts, identifier),
+            args=('mon', hosts, identifier),
+            kwargs=kwargs,
         )
 
         return task
