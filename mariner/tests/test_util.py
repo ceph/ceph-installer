0c109194cf24fddfc4e601f6f6eb994730192191
Alfredo Deza <adeza@redhat.com>
2016-02-10 10:03:54 -0500

    use proper 'self' in methods
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- mariner/tests/test_util.py
+++ mariner/tests/test_util.py
@@ -1,4 +1,3 @@
-import pecan
 import os
 
 from mariner import util
@@ -6,18 +5,18 @@ from mariner import util
 
 class TestGenerateInventoryFile(object):
 
-    def test_single_host(session, tmpdir):
+    def test_single_host(self, tmpdir):
         result = util.generate_inventory_file("mons", "google.com", "uuid", tmp_dir=str(tmpdir))
         with open(result, 'r') as f:
             data = [line.strip() for line in f.readlines()]
             assert "[mons]" in data
             assert "google.com" in data
 
-    def test_correct_filename(session, tmpdir):
+    def test_correct_filename(self, tmpdir):
         result = util.generate_inventory_file("mons", "google.com", "uuid", tmp_dir=str(tmpdir))
         assert "uuid_" in result
 
-    def test_multiple_hosts(session, tmpdir):
+    def test_multiple_hosts(self, tmpdir):
         hosts = ['google.com', 'redhat.com']
         result = util.generate_inventory_file("mons", hosts, "uuid", tmp_dir=str(tmpdir))
         with open(result, 'r') as f:
@@ -26,7 +25,7 @@ class TestGenerateInventoryFile(object):
             assert "google.com" in data
             assert "redhat.com" in data
 
-    def test_tmp_dir(session, tmpdir):
+    def test_tmp_dir(self, tmpdir):
         result = util.generate_inventory_file("mons", "google.com", "uuid", tmp_dir=str(tmpdir))
         assert str(tmpdir) in result
 
