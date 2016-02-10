15397db677f1d959781ee6a012114ad98c722db6
Alfredo Deza <adeza@redhat.com>
2016-02-10 13:01:43 -0500

    robust detection of ssh paths to create keys otherwise
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- mariner/controllers/setup.py
+++ mariner/controllers/setup.py
@@ -1,9 +1,13 @@
 from pecan import expose, request, response
 from webob.static import FileIter
-from mariner.util import make_setup_script
+from mariner.util import make_setup_script, mkdir
 from mariner import process
+from mariner.controllers import error
 import os
 from StringIO import StringIO
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class SetupController(object):
@@ -22,16 +26,25 @@ class SetupController(object):
         # look for the ssh key of the current user
         private_key_path = os.path.expanduser('~/.ssh/id_rsa')
         public_key_path = os.path.expanduser('~/.ssh/id_rsa.pub')
+        ssh_dir = os.path.dirname(public_key_path)
+
+        if not os.path.isdir(ssh_dir):
+            logger.warning('.ssh directory not found, creating one at: %s', ssh_dir)
+            mkdir(ssh_dir)
 
         # if there isn't one create it
         if not os.path.exists(public_key_path):
+            logger.warning('expected public key not found: %s', public_key_path)
+            logger.warning('will create new ssh key pair')
             # create one
             command = [
                     'ssh-keygen', '-q', '-t', 'rsa',
-                    '-N', '""',
+                    '-N', '',
                     '-f', private_key_path,
             ]
-            process.run(command, send_input='y\n')
+            out, err, code = process.run(command, send_input='y\n')
+            if code != 0:
+                error(500, err)
 
         # define the file to download
         response.headers['Content-Disposition'] = 'attachment; filename=id_rsa.pub'
