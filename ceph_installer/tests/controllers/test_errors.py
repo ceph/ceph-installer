fbf87d69b2a89fc1a6f7a42c88ea5acc914d4d34
Andrew Schoen <aschoen@redhat.com>
2016-02-11 09:25:37 -0600

    fix a couple failing tests
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- ceph_installer/tests/controllers/test_errors.py
+++ ceph_installer/tests/controllers/test_errors.py
@@ -15,13 +15,13 @@ class TestSchemaErrors(object):
         message = result.json['message']
         assert "hosts" in message
         assert message.endswith(
-                "failed validation, requires format: ['host1', 'host2']"
+            "failed validation, requires format: ['host1', 'host2']"
         )
 
-    def test_release_is_wrong_type(self, session):
-        params = {'hosts': ['node1'], 'release': False}
+    def test_redhat_storage_is_wrong_type(self, session):
+        params = {'hosts': ['node1'], 'redhat_storage': "foo"}
         result = session.app.post_json("/api/mon/install/", params=params,
                                        expect_errors=True)
         message = result.json['message']
-        assert message.endswith('not of type string')
-        assert 'release' in message
+        assert message.endswith('not of type boolean')
+        assert 'redhat_storage' in message
