dcc51eb1d36c82ef360288b097d5f55964ec5d9c
Andrew Schoen <aschoen@redhat.com>
2016-02-15 10:47:58 -0600

    use skip_tags; set the cwd for the process.run call to ansible-playbook
    Setting the cwd gets around issues related to Ansible not finding action
    plugins that are included in ceph-ansible. Ansible relies on relative
    pathing to find these things, namely the ceph-ansible ansible.cfg file
    that defines where the action plugins live.
    
    See this issue for an example of the problem:
    
    https://github.com/ceph/ceph-ansible/issues/531
    
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- ceph_installer/tasks.py
+++ ceph_installer/tasks.py
@@ -1,3 +1,4 @@
+import os
 import logging
 
 from datetime import datetime
@@ -26,13 +27,20 @@ def call_ansible(component, hosts, identifier, tags="", skip_tags="", extra_vars
         extra_vars = dict()
     component_title = "%s%s" % (component, '' if component.endswith('s') else 's')
     hosts_file = util.generate_inventory_file(component_title, hosts, identifier)
-    command = process.make_ansible_command(hosts_file, identifier, tags=tags, extra_vars=extra_vars)
+    command = process.make_ansible_command(hosts_file, identifier, tags=tags, extra_vars=extra_vars, skip_tags=skip_tags)
     task = models.Task.query.filter_by(identifier=identifier).first()
     task.command = ' '.join(command)
     task.started = datetime.now()
     # force a commit here so we can reference this command later if it fails
     models.commit()
-    out, err, exit_code = process.run(command)
+    working_dir = util.get_playbook_path()
+    working_dir = os.path.split(working_dir)[0]
+    # ansible depends on relative pathing to figure out how to load
+    # plugins, among other things. Setting the current working directory
+    # for this subprocess call to the directory where the playbook resides
+    # allows ansible to properly find action plugins defined in ceph-ansible.
+    kwargs = dict(cwd=working_dir)
+    out, err, exit_code = process.run(command, **kwargs)
     task.succeeded = not exit_code
     task.exit_code = exit_code
     task.stdout = out
