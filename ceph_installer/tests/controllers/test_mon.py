73157e9d4c4af0ca8d8376dfb6492cbc9f7b6bed
Andrew Schoen <aschoen@redhat.com>
2016-02-16 10:03:40 -0600

    tests for api/mon/configure
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- ceph_installer/tests/controllers/test_mon.py
+++ ceph_installer/tests/controllers/test_mon.py
@@ -31,3 +31,17 @@ class TestMonController(object):
                                        expect_errors=True)
         assert result.json['endpoint'] == '/api/mon/install/'
         assert result.json['identifier'] is not None
+
+    def test_configure_missing_fields(self, session):
+        data = dict()
+        result = session.app.post_json("/api/mon/configure/", params=data,
+                                       expect_errors=True)
+        assert result.status_int == 400
+
+    def test_configure_hosts(self, session, monkeypatch):
+        monkeypatch.setattr(mon.call_ansible, 'apply_async', lambda args, kwargs: None)
+        data = dict(host="node1", monitor_interface="eth0", fsid="1720107309134")
+        result = session.app.post_json("/api/mon/configure/", params=data,
+                                       expect_errors=True)
+        assert result.json['endpoint'] == '/api/mon/configure/'
+        assert result.json['identifier'] is not None
