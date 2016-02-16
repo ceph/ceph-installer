97fa26dde424bea7c540d10433e44c672e0785b6
Alfredo Deza <adeza@redhat.com>
2016-02-16 10:00:54 -0500

    agent endpoint to allow POST at index method
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_installer/controllers/agent.py
+++ ceph_installer/controllers/agent.py
@@ -16,17 +16,13 @@ logger = logging.getLogger(__name__)
 
 class AgentController(object):
 
-    @expose('json')
-    def index(self):
-        return dict()
-
     @expose(generic=True, template='json')
-    def install(self):
+    def index(self):
         error(405)
 
-    @install.when(method='POST', template='json')
+    @index.when(method='POST', template='json')
     @validate(schemas.install_schema, handler="/errors/schema")
-    def install_post(self):
+    def install(self):
         hosts = request.json.get('hosts')
         extra_vars = util.get_install_extra_vars(request.json)
         identifier = str(uuid4())
@@ -37,34 +33,7 @@ class AgentController(object):
         # we need an explicit commit here because the command may finish before
         # we conclude this request
         models.commit()
-        kwargs = dict(extra_vars=extra_vars, tags="package-install")
-        call_ansible.apply_async(
-            args=('agent', hosts, identifier),
-            kwargs=kwargs,
-        )
-
-        return task
-
-    @expose(generic=True, template='json')
-    def configure(self):
-        error(405)
-
-    # we need validation here
-    @configure.when(method='POST', template='json')
-    def configure_post(self):
-        hosts = [request.json.get('host')]
-        # even with configuring we need to tell ceph-ansible
-        # if we're working with upstream ceph or red hat ceph storage
-        extra_vars = util.get_install_extra_vars(request.json)
-        identifier = str(uuid4())
-        task = models.Task(
-            identifier=identifier,
-            endpoint=request.path,
-        )
-        # we need an explicit commit here because the command may finish before
-        # we conclude this request
-        models.commit()
-        kwargs = dict(extra_vars=extra_vars, skip_tags="package-install")
+        kwargs = dict(extra_vars=extra_vars)
         call_ansible.apply_async(
             args=('agent', hosts, identifier),
             kwargs=kwargs,
