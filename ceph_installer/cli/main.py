664cdbf586b4e2ba5f6821a4998492319160bbdd
Alfredo Deza <adeza@redhat.com>
2016-02-17 13:51:04 -0500

    parse the new version in setup.py
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ ceph_installer/cli/main.py
@@ -0,0 +1,73 @@
+import pkg_resources
+import sys
+
+from tambo import Transport
+import ceph_installer
+from ceph_installer.cli import log
+from ceph_installer.cli import dev
+from ceph_installer.cli.decorators import catches
+
+
+class CephInstaller(object):
+    _help = """
+A command line utility to install and configure Ceph using an HTTP API as a REST service
+to call Ansible.
+
+Version: %s
+
+Global Options:
+-h, --help, help    Show this program's help menu
+--log, --logging    Set the level of logging. Acceptable values:
+                    debug, warning, error, critical
+
+
+%s
+    """
+
+    mapper = {'dev': dev.Dev}
+
+    def __init__(self, argv=None, parse=True):
+        self.plugin_help = "No plugins found/loaded"
+        if argv is None:
+            argv = sys.argv
+        if parse:
+            self.main(argv)
+
+    def help(self, subhelp):
+        version = ceph_installer.__version__
+        return self._help % (version, subhelp)
+
+    @catches(KeyboardInterrupt)
+    def main(self, argv):
+        import logging
+        # Set console logging first with some defaults, to prevent having exceptions
+        # before hitting logging configuration. The defaults can/will get overridden
+        # later.
+
+        # Console Logger
+        sh = logging.StreamHandler()
+        sh.setFormatter(log.color_format())
+        sh.setLevel(logging.WARNING)
+
+        # because we're in a module already, __name__ is not the ancestor of
+        # the rest of the package; use the root as the logger for everyone
+        root_logger = logging.getLogger()
+
+        # allow all levels at root_logger, handlers control individual levels
+        root_logger.setLevel(logging.DEBUG)
+        root_logger.addHandler(sh)
+
+        options = [['--log', '--logging']]
+        parser = Transport(argv, mapper=self.mapper,
+                           options=options, check_help=False,
+                           check_version=False)
+        parser.parse_args()
+        #delgado.config['verbosity'] = parser.get('--log', 'info')
+        parser.catch_help = self.help(parser.subhelp())
+        parser.catch_version = ceph_installer.__version__
+        parser.mapper = self.mapper
+        if len(argv) <= 1:
+            return parser.print_help()
+        parser.dispatch()
+        parser.catches_help()
+        parser.catches_version()
--- setup.py
+++ setup.py
@@ -6,8 +6,14 @@ except ImportError:
     from ez_setup import use_setuptools
     use_setuptools()
     from setuptools import setup, find_packages, Command
+import re
 
-version = '0.2.0'
+
+module_file = open("ceph_installer/__init__.py").read()
+metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", module_file))
+long_description = open('README.rst').read()
+version = metadata['version']
+
 
 class ReleaseCommand(Command):
     """ Tag and push a new release. """
@@ -41,18 +47,22 @@ class ReleaseCommand(Command):
         print ' '.join(cmd)
         subprocess.check_call(cmd)
 
+
 setup(
     name='ceph-installer',
     version=version,
     description='An HTTP API that provides Ceph installation/configuration endpoints',
+    long_description=long_description,
     author='',
     author_email='',
+    scripts=['bin/ceph-installer'],
     install_requires=[
         'pecan>=1',
         'celery',
         'sqlalchemy',
         'gunicorn',
         'pecan-notario',
+        'tambo',
     ],
     zip_safe=False,
     include_package_data=True,
@@ -61,5 +71,5 @@ setup(
         [pecan.command]
         populate=ceph_installer.commands.populate:PopulateCommand
         """,
-    cmdclass={ 'release': ReleaseCommand },
+    cmdclass={'release': ReleaseCommand},
 )
