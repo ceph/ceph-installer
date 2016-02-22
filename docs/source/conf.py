9bc62e13b24bb867dd64e99b340be731c1619484
Alfredo Deza <adeza@redhat.com>
2016-02-22 14:16:36 -0500

    comment out cruft; use a light style for code
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,7 +83,7 @@ exclude_patterns = []
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'solarized.LightStyle'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
@@ -93,15 +93,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'flask_small'
+#html_theme = ''
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'index_logo':       'notario.jpg',
-    'github_fork':      'alfredodeza/notario'
-}
+#html_theme_options = { }
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_themes']
