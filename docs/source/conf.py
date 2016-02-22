e2de8de72cfc86a71a371699ef4504501a525bfc
Alfredo Deza <adeza@redhat.com>
2016-02-22 14:21:49 -0500

    add the httpdomain extension
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ import ceph_installer
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-#extensions = ['sphinx.ext.doctest', 'sphinx.ext.autodoc']
+extensions = ['sphinxcontrib.httpdomain']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
