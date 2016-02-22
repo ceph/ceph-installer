9a91fff2097f256436cc4cc25b67b1dbe79465ee
Alfredo Deza <adeza@redhat.com>
2016-02-22 14:22:56 -0500

    Define the name of the api
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,7 @@ import ceph_installer
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinxcontrib.httpdomain']
+http_index_localname = "ceph-installer HTTP API"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
