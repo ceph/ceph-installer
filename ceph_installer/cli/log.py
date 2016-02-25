f12a149e907c4d787ee58eef8ca4bf0db31717ba
Alfredo Deza <adeza@redhat.com>
2016-02-25 12:37:12 -0500

    improve formatting of log
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_installer/cli/log.py
+++ ceph_installer/cli/log.py
@@ -2,15 +2,19 @@
 This is really not a logging facility. For the CLI we only require a nice color
 output on some interactions. Full logging is not needed.
 """
+import sys
 from ceph_installer.cli import util
+
 
 def error(message):
     line = "%s %s\n" % (util.red_arrow, message)
     sys.stderr.write(line)
 
+
 def debug(message):
     line = "%s %s\n" % (util.blue_arrow, message)
     sys.stdout.write(line)
+
 
 def info(message):
     line = "%s %s\n" % (util.bold_arrow, message)
