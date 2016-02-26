d6191dd4308fc9acbb15c6f67474b09c60d53c1e
Alfredo Deza <adeza@redhat.com>
2016-02-26 15:39:09 -0500

    add tests for the call_ansible task
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ ceph_installer/tests/test_tasks.py
@@ -0,0 +1,23 @@
+from ceph_installer import models, tasks
+
+
+class TestCallAnsible(object):
+
+    def setup(self):
+        task = models.Task(
+            identifier='aaaa',
+            endpoint='/api/test/',
+        )
+        models.commit()
+
+    def test_set_exit_code_on_error(self, session):
+        tasks.call_ansible.apply(args=([], 'aaaa')).get()
+        task = models.Task.get(1)
+        assert task.exit_code == -1
+
+    def test_set_exception_on_stderr(self, session, monkeypatch):
+        def error(*a, **kw): raise OSError('a severe error')
+        monkeypatch.setattr(tasks.process, 'run', error)
+        tasks.call_ansible.apply(args=([], 'aaaa')).get()
+        task = models.Task.get(1)
+        assert task.stderr == 'a severe error'
