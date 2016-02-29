478fa936cd1451405c11fac4ae8f960a4491b5e6
Alfredo Deza <adeza@redhat.com>
2016-02-29 10:58:52 -0500

    handle all errors, format exceptions that were not caught
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_installer/cli/main.py
+++ ceph_installer/cli/main.py
@@ -36,7 +36,7 @@ Global Options:
         version = ceph_installer.__version__
         return self._help % (version, subhelp)
 
-    @catches(KeyboardInterrupt, logger=log)
+    @catches(KeyboardInterrupt, handle_all=True, logger=log)
     def main(self, argv):
         parser = Transport(argv, mapper=self.mapper,
                            options=[], check_help=False,
