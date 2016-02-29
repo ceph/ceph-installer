b820355bda01466d4e36dde370837d18cea314fd
Alfredo Deza <adeza@redhat.com>
2016-02-29 17:55:06 -0500

    tests for the agent controller
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_installer/tests/controllers/test_agent.py
+++ ceph_installer/tests/controllers/test_agent.py
@@ -12,25 +12,20 @@ class TestAgentController(object):
             fsid="1720107309134",
         )
 
-    def test_index_get_is_allowed(self, session):
+    def test_index_get_is_not_allowed(self, session):
         result = session.app.get("/api/agent/", expect_errors=True)
-        assert result.status_int == 200
-
-    def test_install_get_is_not_allowed(self, session):
-        result = session.app.get("/api/agent/install/", expect_errors=True)
         assert result.status_int == 405
 
-    def test_install_incorrect_schema(self, session):
+    def test_index_incorrect_schema(self, session):
         data = dict(hosts=["google.com"], bogus="foo")
-        result = session.app.post_json("/api/agent/install/", params=data,
+        result = session.app.post_json("/api/agent/", params=data,
                                        expect_errors=True)
         assert result.status_int == 400
 
-    def test_install_works_with_right_schema(self, session, monkeypatch):
+    def test_index_post_works_with_right_schema(self, session, monkeypatch):
         monkeypatch.setattr(agent.call_ansible, 'apply_async', lambda args, kwargs: None)
         data = dict(hosts=["node1"])
-        result = session.app.post_json("/api/agent/install/", params=data,
-                                       expect_errors=True)
-        assert result.json['endpoint'] == '/api/agent/install/'
+        result = session.app.post_json("/api/agent/", params=data)
+        assert result.json['endpoint'] == '/api/agent/'
         assert result.json['identifier'] is not None
 
