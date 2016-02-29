22c759a7a4788e960d1d7342f97deab5cb7bdca2
Alfredo Deza <adeza@redhat.com>
2016-02-29 08:36:18 -0500

    patch make_ansible_command in tests
    So that if ansible is not installed this doesn't break the test
    
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_installer/tests/test_tasks.py
+++ ceph_installer/tests/test_tasks.py
@@ -10,7 +10,12 @@ class TestCallAnsible(object):
         )
         models.commit()
 
-    def test_set_exit_code_on_error(self, session):
+    def test_set_exit_code_on_error(self, session, monkeypatch):
+        monkeypatch.setattr(
+            tasks.process,
+            'make_ansible_command',
+            lambda *a, **kw: ['echo']
+        )
         tasks.call_ansible.apply(args=([], 'aaaa')).get()
         task = models.Task.get(1)
         assert task.exit_code == -1
@@ -18,6 +23,11 @@ class TestCallAnsible(object):
     def test_set_exception_on_stderr(self, session, monkeypatch):
         def error(*a, **kw): raise OSError('a severe error')
         monkeypatch.setattr(tasks.process, 'run', error)
+        monkeypatch.setattr(
+            tasks.process,
+            'make_ansible_command',
+            lambda *a, **kw: ['echo']
+        )
         tasks.call_ansible.apply(args=([], 'aaaa')).get()
         task = models.Task.get(1)
         assert task.stderr == 'a severe error'
