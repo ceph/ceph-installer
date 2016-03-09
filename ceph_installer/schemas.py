e2bf5c86d7b89cbc8c2f2371a9dbc86aaefa8d3e
Alfredo Deza <adeza@redhat.com>
2016-03-09 14:54:49 -0500

    remove redhat cdn from the agent install schema, not needed
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_installer/schemas.py
+++ ceph_installer/schemas.py
@@ -28,8 +28,6 @@ install_schema = (
 agent_install_schema = (
     ("hosts", list_of_hosts),
     (optional("master"), types.string),
-    (optional("redhat_storage"), types.boolean),
-    (optional("redhat_use_cdn"), types.boolean),
 )
 
 mon_configure_schema = (
