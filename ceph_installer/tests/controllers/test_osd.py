742e04274522f00117d1dda73e6cb0213fa1237e
Alfredo Deza <adeza@redhat.com>
2017-01-17 16:17:14 -0500

    tests: verify that the osd controller can configure collocated journals
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- ceph_installer/tests/controllers/test_osd.py
+++ ceph_installer/tests/controllers/test_osd.py
@@ -147,6 +147,85 @@ class TestOSDController(object):
         assert extra_vars["cluster"] == "lol"
 
 
+class TestOSDCollocatedController(object):
+
+    def setup(self):
+        data = dict(
+            host="node1",
+            fsid="1720107309134",
+            devices=['/dev/sdb', '/dev/sda'],
+            monitors=[{"host": "mon1.host", "address": "10.0.0.1"}],
+            journal_size=100,
+            public_network="0.0.0.0/24",
+        )
+        self.configure_data = data
+
+    def test_configure_devices_is_wrong_type(self, session, monkeypatch):
+        monkeypatch.setattr(osd.call_ansible, 'apply_async', lambda args, kwargs: None)
+        data = self.configure_data.copy()
+        data['devices'] = "/dev/sdb"
+        result = session.app.post_json("/api/osd/configure/", params=data,
+                                       expect_errors=True)
+        assert result.status_int == 400
+        message = result.json["message"]
+        assert 'devices' in message
+        assert 'not of type dictionary or list' in message
+
+    def test_configure_success(self, session, monkeypatch):
+        monkeypatch.setattr(osd.call_ansible, 'apply_async', lambda args, kwargs: None)
+        result = session.app.post_json("/api/osd/configure/", params=self.configure_data)
+        assert result.status_int == 200
+        assert result.json['endpoint'] == '/api/osd/configure/'
+        assert result.json['identifier'] is not None
+
+    def test_configure_playbook(self, session, monkeypatch):
+        def check(args, kwargs):
+            assert "osd-configure.yml" in kwargs["playbook"]
+
+        monkeypatch.setattr(osd.call_ansible, 'apply_async', check)
+        result = session.app.post_json("/api/osd/configure/", params=self.configure_data)
+        assert result.status_int == 200
+
+    def test_configure_with_conf(self, session, monkeypatch):
+        monkeypatch.setattr(osd.call_ansible, 'apply_async', lambda args, kwargs: None)
+        self.configure_data['conf'] = {"global": {"auth supported": "cephx"}}
+        result = session.app.post_json("/api/osd/configure/", params=self.configure_data)
+        assert result.json['endpoint'] == '/api/osd/configure/'
+        assert result.json['identifier'] is not None
+
+    def test_configure_with_invalid_conf(self, session, monkeypatch):
+        monkeypatch.setattr(osd.call_ansible, 'apply_async', lambda args, kwargs: None)
+        self.configure_data['conf'] = {"global": {"auth supported": "cephx"}, "monn": 1}
+        result = session.app.post_json("/api/osd/configure/", params=self.configure_data,
+                                       expect_errors=True)
+        assert result.status_int == 400
+        assert "unexpected item in data" in result.json["message"]
+
+    def test_configure_monitor_name_is_set(self, session, monkeypatch, argtest):
+        monkeypatch.setattr(
+            osd.call_ansible, 'apply_async', argtest)
+        self.configure_data['host'] = "mon1.host"
+        session.app.post_json("/api/osd/configure/", params=self.configure_data)
+        kwargs = argtest.kwargs['kwargs']
+        assert kwargs['extra_vars']['monitor_name'] == "mon1.host"
+
+    def test_configure_monitor_name_is_not_set(self, session, monkeypatch, argtest):
+        monkeypatch.setattr(
+            osd.call_ansible, 'apply_async', argtest)
+        session.app.post_json("/api/osd/configure/", params=self.configure_data)
+        kwargs = argtest.kwargs['kwargs']
+        assert "monitor_name" not in kwargs['extra_vars']
+
+    def test_configure_with_cluster_name(self, session, monkeypatch, argtest):
+        monkeypatch.setattr(
+            osd.call_ansible, 'apply_async', argtest)
+        self.configure_data['cluster_name'] = "lol"
+        session.app.post_json("/api/osd/configure/", params=self.configure_data)
+        extra_vars = argtest.kwargs['kwargs']['extra_vars']
+        assert "cluster" in extra_vars
+        assert "cluster_name" not in extra_vars
+        assert extra_vars["cluster"] == "lol"
+
 class TestOsdVerbose(object):
 
     def setup(self):
